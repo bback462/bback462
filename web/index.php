bcc4f74fdd41fb687ad9502f1e369a9127147d75
josephworkoff <54723843+josephworkoff@users.noreply.github.com>
2020-02-05 12:23:08 -0500

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -22,6 +22,31 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/profile', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('profile.twig');
+});
+
+$app->get('/login', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('login.twig');
+});
+
+$app->get('/matches', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('matches.twig');
+});
+
+$app->get('/search', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('search.twig');
+});
+
+$app->get('/faq', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('faq.twig');
+});
+
 $app->run();
 
 
