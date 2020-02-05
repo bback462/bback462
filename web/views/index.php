762b22e63ce20666c30dd59e8ab798e9155fe400
bback462 <60359318+bback462@users.noreply.github.com>
2020-02-05 17:33:46 -0500

    test

--- web/views/index.php
+++ web/views/index.php
@@ -1,6 +1,43 @@
 <?php
+require_once("views/header.html");
+<div class="jumbotron text-center">
+  <div class="container">
 
-require('../vendor/autoload.php');
+<!--
+    <a href="/" class="lang-logo">
+      <img src="/lang-logo.png">
+    </a>
+-->
+  <h1>Bear Link</h1>
+  <p>HOME</p>
+
+
+
+  </div>
+</div>
+
+<div class="jumbotron text-center" th:fragment="footer">
+  <a type="button" class="btn btn-lg btn-primary" href="/"><span class="glyphicon glyphicon-home"></span> HOME</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/profile"><span class="glyphicon glyphicon-user"></span> PROFILE</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/matches"><span class="glyphicon glyphicon-heart"></span> MATCHES</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/search"><span class="glyphicon glyphicon-search"></span> SEARCH</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/faq"><span class="glyphicon glyphicon-list"></span> FAQ</a>
+
+  <p>&copy; 2020 Bear Link</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+?>
+/*require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
