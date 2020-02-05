5ef222a202d88997a888c6efca7e8032c5c3bda6
bback462 <60359318+bback462@users.noreply.github.com>
2020-02-05 15:34:28 -0500

    added DBconnection.php

---
+++ web/DBconnection.php
@@ -0,0 +1,22 @@
+<?php
+
+$host        = "ec2-54-197-34-207.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d4msuht8eli7mu";
+   $credentials = "user = vjmnjxjipcxrhd password=df99778cb4f95c81e3fe170d6de3e2c6a426fbcf5838d6f2d16015e2c2395949
+";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
