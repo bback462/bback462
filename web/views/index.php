ce372ae0f83f023b19a29731d259a49711922391
bback462 <60359318+bback462@users.noreply.github.com>
2020-02-05 17:37:09 -0500

    Revert "1"
    This reverts commit 6fd7bbca4196d11b1af0dbfcba37dfc3474493d5.

--- web/views/index.php
+++ web/views/index.php
@@ -85,3 +85,14 @@ $app->get('/faq', function() use($app) {
 });
 
 $app->run();
+
+
+
+
+
+
+// DELETE COWSAY
+//$app->get('/cowsay', function() use($app) {
+//  $app['monolog']->addDebug('cowsay');
+//  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+//});
