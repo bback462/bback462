2b074f4e5e8a0f3f3e389567a514d523cd0e1a8e
bback462 <60359318+bback462@users.noreply.github.com>
2020-02-05 14:10:33 -0500

    test changing from .twig to .php

---
+++ web/views/index.php
@@ -0,0 +1,31 @@
+{% extends "layout.html" %}
+
+{% block content %}
+
+<div class="jumbotron text-center">
+  <div class="container">
+
+<!--
+    <a href="/" class="lang-logo">
+      <img src="/lang-logo.png">
+    </a>
+-->
+  <h1>Bear Link</h1>
+  <p>HOME</p>
+
+
+
+  </div>
+</div>
+
+<div class="jumbotron text-center" th:fragment="footer">
+  <a type="button" class="btn btn-lg btn-primary" href="/"><span class="glyphicon glyphicon-home"></span> HOME</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/profile"><span class="glyphicon glyphicon-user"></span> PROFILE</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/web/views/matches.twig"><span class="glyphicon glyphicon-heart"></span> MATCHES</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/search"><span class="glyphicon glyphicon-search"></span> SEARCH</a>
+  <a type="button" class="btn btn-lg btn-primary" href="/faq"><span class="glyphicon glyphicon-list"></span> FAQ</a>
+
+  <p>&copy; 2020 Bear Link</p>
+</div>
+
+{% endblock %}
