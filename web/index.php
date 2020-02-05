196f09990910b528fe23c8d0c2d349c97d118dd5
bback462 <60359318+bback462@users.noreply.github.com>
2020-02-05 13:50:39 -0500

    test

--- web/index.php
+++ web/index.php
@@ -47,6 +47,10 @@ $app->get('/faq', function() use($app) {
   return $app['twig']->render('faq.twig');
 });
 
+$app->get('/faq', function ($request, $response, $args) {
+    return $this->view->render($response, 'faq.html.twig');
+});
+
 $app->run();
 
 
